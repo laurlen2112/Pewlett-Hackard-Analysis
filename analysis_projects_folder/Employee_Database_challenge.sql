0d85fbd5f93fbab402c8a24c3b31b2dfd2a200fa
laurlen2112 <102885345+laurlen2112@users.noreply.github.com>
2022-05-25 14:38:12 -0400

    Add files via upload
    Upload challenge code to show suggested queries in README.
    
    Please note:
    
    I experienced difficulty in running the query for deliverable 2 since my results in the “title” column would differ from the example in the protocol for one or two employees.  I spent over an hour working with a learning assistant (ask-150923) who confirmed that my query and table schemas were correct.  The LA suggested that the example CSV provided in the protocol may be incorrect because the current job title (per the “Title” CSV) for the employee in row 8 is Sr. Engineer, however, the example shows that employee’s job title as Engineer.
    
    I continued to play around with the query and finally received the same result as the example in the protocol by using the employee number from the “Titles” table as my “distinct on” filter.  However, on subsequent runs of that query I noticed that it also returned results that were inconsistent with the example.

--- analysis_projects_folder/Employee_Database_challenge.sql
+++ analysis_projects_folder/Employee_Database_challenge.sql
@@ -43,7 +43,7 @@ ORDER BY COUNT DESC;
 --check table
 SELECT * FROM retiring_titles;
 
---Deliverable 2
+--DELIVERABLE 2
 
 SELECT DISTINCT ON (ti.emp_no) e.emp_no,
   e.first_name,
@@ -63,3 +63,52 @@ AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY ti.emp_no;
 
 SELECT * FROM mentorship
+
+--PLEASE NOTE:
+
+--I experienced difficulty in running the query for deliverable 2 since my results in the “title” column 
+--would differ from the example in the protocol for one or two employees on each run.  I spent over an hour 
+--working with a learning assistant (ask-150923) who confirmed that my query and table schemas were correct.  
+--The LA suggested that the example CSV provided in the protocol may be incorrect because the current 
+--job title (per the “Title” CSV) for the employee in row 8 is Sr. Engineer, however, the example shows 
+--that employee’s job title as Engineer.
+
+--I continued to play around with the query and finally received the same result as the example in the protocol 
+--by using the employee number from the “Titles” table as my “distinct on” filter.  However, on subsequent runs 
+--of that query I noticed that it also returned results that were inconsistent with the example. 
+
+
+
+--SUGGESTED FUTURE QUERIES AS DISCUSSED IN THE README ANALYSIS:
+
+--SUGGESTION 1) CREATE A MENTORSHIP TABLE SIMILAR TO THE RETIRING TITLES TABLE
+SELECT COUNT(mt.title), mt.title
+INTO mentorship_titles
+FROM mentorship as mt
+GROUP BY title
+ORDER BY COUNT DESC;
+
+--SUGGESTION 2) drill down on the retiring_titles information:
+
+--2A) create table that shows unique title and dept no.
+SELECT ut.emp_no, 
+	   ut.first_name,
+	   ut.last_name,
+	   ut.title, 
+	   de.dept_no
+INTO drill_down
+	FROM unique_titles as ut
+	INNER JOIN dept_emp as de
+	ON (ut.emp_no = de.emp_no)
+ORDER BY emp_no;
+
+--2B) count of unique titles and dept number
+
+SELECT COUNT(dd.dept_no), dd.title, dd.dept_no
+INTO drill_count_2
+FROM drill_down as dd
+GROUP BY dept_no, title
+ORDER BY COUNT DESC;
+
+
+
